879db6b562efee0f1f54128ab8f74d91585c4f85
TheCreator <31379149+Flurbos@users.noreply.github.com>
2017-10-29 07:26:16 +0800

    Rename ClientName

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both flurbod and flurbo-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Flurbo");
 
 /**
  * Client version number
