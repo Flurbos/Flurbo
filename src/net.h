c6a6eaf63ae020bc9f719daee3a39090cfb5cb34
TheCreator <31379149+Flurbos@users.noreply.github.com>
2017-11-01 16:50:11 +0800

    Default_ForceDnsSeed enabled - until next update

--- src/net.h
+++ src/net.h
@@ -66,7 +66,7 @@ static const uint64_t DEFAULT_MAX_UPLOAD_TARGET = 0;
 /** Default for blocks only*/
 static const bool DEFAULT_BLOCKSONLY = false;
 
-static const bool DEFAULT_FORCEDNSSEED = false;
+static const bool DEFAULT_FORCEDNSSEED = true;//false - Will turn back to false in next update, this is to fix our network!
 static const size_t DEFAULT_MAXRECEIVEBUFFER = 5 * 1000;
 static const size_t DEFAULT_MAXSENDBUFFER    = 1 * 1000;
 
